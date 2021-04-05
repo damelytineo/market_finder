336dc76226c11e0d9be21dae7493352817babde9
Mely07 <damely.tineo07@gmail.com>
2021-04-05 17:45:18 -0400

    Set up backend show route & initial frontend files.

--- market_finder_api/app/controllers/users_controller.rb
+++ market_finder_api/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show]
+
+    # GET /users/1
+    def show
+        render json: @user
+    end
+
+    
+    # POST /users
+    def create
+        @user = User.new(user_params)
+    end
+
+
+    private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
