f4d17b2a9da07b67af9366e441a26d85a9035ce4
Mely07 <damely.tineo07@gmail.com>
2021-04-05 12:16:47 -0400

    Initial setup of models & associations.

---
+++ market_finder_api/db/migrate/20210405144618_create_markets.rb
@@ -0,0 +1,18 @@
+class CreateMarkets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :markets do |t|
+      t.string :name
+      t.string :borough
+      t.string :street_address
+      t.integer :district
+      t.float :latitude
+      t.float :longitude
+      t.string :days_of_operation 
+      t.string :hours
+      t.string :season_dates
+
+      t.belongs_to :user
+      t.timestamps
+    end
+  end
+end
