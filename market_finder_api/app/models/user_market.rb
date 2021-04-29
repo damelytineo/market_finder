9a304abd6a8d429a66b5953a257e30466bf8c79f
Mely07 <damely.tineo07@gmail.com>
2021-04-28 23:14:09 -0400

    Initial setup for RSpec or Minitest.

--- market_finder_api/app/models/user_market.rb
+++ market_finder_api/app/models/user_market.rb
@@ -1,4 +1,6 @@
 class UserMarket < ApplicationRecord
     belongs_to :user
     belongs_to :market
+
+    validates_presence_of :user_id, :market_id
 end
--- market_finder_api/test/models/user_market_test.rb
+++ market_finder_api/test/models/user_market_test.rb
@@ -4,4 +4,19 @@ class UserMarketTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user_market = UserMarket.new
+  end
+
+  # test "market should be valid" do 
+  #   assert @user_market.valid?
+  # end
+
+  test "market should have a user id" do 
+    # alongside model validations 
+    @user_market.user_id= ""
+    assert_not @user_market.valid?
+  end
+
 end
