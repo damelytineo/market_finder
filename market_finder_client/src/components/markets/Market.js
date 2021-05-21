8390f3eff9495a1549e21a316c9f42f979f5e414
Mely07 <damely.tineo07@gmail.com>
2021-05-20 23:57:49 -0400

    Render corresponding market via match & markets props.

--- market_finder_client/src/components/markets/Market.js
+++ market_finder_client/src/components/markets/Market.js
@@ -1,10 +1,15 @@
 
 import React from 'react';
 
-const Market = (match, markets) => {
-    console.log("match")
+const Market = ({ match, markets }) => {
+
+    if (markets.length === 0) return null
+
+    const market = markets.find(market => market.id == match.params.market_id)
+
     return (
         <div>
+            <h5>{market.name}</h5>
         </div>
     );
 };
