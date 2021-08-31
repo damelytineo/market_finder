283e27253ffd566973747d548976785b9b834643
Mely07 <damely.tineo07@gmail.com>
2021-08-31 00:17:41 -0400

    Converted Markets to functional component with useState hook.

--- market_finder_client/src/components/markets/Markets.js
+++ market_finder_client/src/components/markets/Markets.js
@@ -1,48 +1,88 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import MarketCard from '../markets/MarketCard';
+// import Form from 'react-bootstrap/Form';
+
+
+// class Markets extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             filteredMarkets: [],
+//         };
+//     }
+
+//     filterByBorough = (borough) => {
+//         this.setState({ filteredMarkets: this.props.markets.filter(market => market.borough === borough) });
+//     }
+
+//     handleOnChange = (e) => {
+//         let borough = (e.target.value);
+//         this.filterByBorough(borough);
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <Form>
+//                     <Form.Select onChange={this.handleOnChange} defaultValue={'default'}>
+//                         <option value="default" disabled="disabled"> Filter by Borough </option>
+//                         <option value="Brooklyn">Brooklyn</option>
+//                         <option value="Queens">Queens</option>
+//                         <option value="Manhattan">Manhattan</option>
+//                         <option value="Bronx">Bronx</option>
+//                         <option value="Staten Island">Staten Island</option>
+//                     </Form.Select>
+//                 </Form>
+//                 {this.state.filteredMarkets.map(market =>
+//                     <div key={market.id}>
+//                         <MarketCard market={market} userMarkets={this.props.userMarkets} />
+//                         <hr />
+//                     </div>
+//                 )}
+//             </div>
+//         );
+//     }
+// }
+
+// export default Markets;
+
+///Display all markets before filtering?
+import React, { useState } from 'react';
 import MarketCard from '../markets/MarketCard';
 import Form from 'react-bootstrap/Form';
 
 
-class Markets extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            filteredMarkets: [],
-        };
+const Markets = (props) => {
+    let [filteredMarkets, setFilteredMarkets] = useState([]);
+
+    const filterByBorough = (borough) => {
+        setFilteredMarkets(props.markets.filter(market => market.borough === borough));
     }
 
-    filterByBorough = (borough) => {
-        this.setState({ filteredMarkets: this.props.markets.filter(market => market.borough === borough) });
-    }
-
-    handleOnChange = (e) => {
+    const handleOnChange = (e) => {
         let borough = (e.target.value);
-        this.filterByBorough(borough);
+        filterByBorough(borough);
     }
-
-    render() {
-        return (
-            <div>
-                <Form>
-                    <Form.Select onChange={this.handleOnChange} defaultValue={'default'}>
-                        <option value="default" disabled="disabled"> Filter by Borough </option>
-                        <option value="Brooklyn">Brooklyn</option>
-                        <option value="Queens">Queens</option>
-                        <option value="Manhattan">Manhattan</option>
-                        <option value="Bronx">Bronx</option>
-                        <option value="Staten Island">Staten Island</option>
-                    </Form.Select>
-                </Form>
-                {this.state.filteredMarkets.map(market =>
-                    <div key={market.id}>
-                        <MarketCard market={market} userMarkets={this.props.userMarkets} />
-                        <hr />
-                    </div>
-                )}
-            </div>
-        );
-    }
-}
+    return (   
+        <div>
+            <Form>
+                <Form.Select onChange={handleOnChange} defaultValue={'default'}>
+                    <option value="default" disabled="disabled"> Filter by Borough </option>
+                    <option value="Brooklyn">Brooklyn</option>
+                    <option value="Queens">Queens</option>
+                    <option value="Manhattan">Manhattan</option>
+                    <option value="Bronx">Bronx</option>
+                    <option value="Staten Island">Staten Island</option>
+                </Form.Select>
+            </Form>
+            {filteredMarkets.map(market =>
+                <div key={market.id}>
+                    <MarketCard market={market} userMarkets={props.userMarkets} />
+                    <hr />
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default Markets;
-
