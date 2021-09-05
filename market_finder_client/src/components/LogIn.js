24211677aaef41e6c56cff66e9b5204c0bb34db0
Mely07 <damely.tineo07@gmail.com>
2021-09-05 10:11:35 -0400

    Converted Login component to functional to use useState hook.

--- market_finder_client/src/components/LogIn.js
+++ market_finder_client/src/components/LogIn.js
@@ -1,24 +1,82 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+
+// class LogIn extends Component {
+//     state = {
+//         username: '',
+//         password: '',
+//     }
+
+//     onChange = (e) => {
+//         let name = e.target.name
+//         this.setState({ [name]: e.target.value })
+//     }
+
+//     onClick = (e) => {
+//         e.preventDefault();
+
+//         let configObj = {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "application/json"
+//             },
+//             credentials: "include", //sends cross-origin cookie
+//             body: JSON.stringify(this.state)
+//         }
+
+//         fetch('http://localhost:3000/login', configObj)
+//             .then(response => response.json())
+//             .then(userData => {
+//                 //lifting state up tp parent (App) 
+//                 //once user signs in we want to make a fetch request to logged_in to set current_user 
+//                 this.props.handleLogin(userData);
+//             });
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <Form>
+//                     <Row>
+//                         <Col xs={3}>
+//                             <Form.Control type="text" placeholder="Username" value={this.state.username} onChange={this.onChange} name="username" />
+//                         </Col>
+//                     </Row>
+//                     <Row>
+//                         <Col xs={3} >
+//                             <Form.Control type="text" placeholder="Password" value={this.state.password} onChange={this.onChange} name="password" />
+//                             <Button variant="success" onClick={(event) => this.onClick(event)}> Submit </Button>
+//                         </Col>
+//                     </Row>
+//                 </Form>
+//             </div>
+//         );
+//     }
+// }
+
+// export default LogIn;
+
+
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 
-class LogIn extends Component {
-    state = {
-        username: '',
-        password: '',
-    }
+const LogIn = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    onChange = (e) => {
-        let name = e.target.name
-        this.setState({ [name]: e.target.value })
-    }
-
-    onClick = (e) => {
+    const onClick = (e) => {
+        console.log(username);
+        console.log(password)
         e.preventDefault();
-
         let configObj = {
             method: "POST",
             headers: {
@@ -26,37 +84,33 @@ class LogIn extends Component {
                 "Accept": "application/json"
             },
             credentials: "include", //sends cross-origin cookie
-            body: JSON.stringify(this.state)
+            body: JSON.stringify({ username, password })
         }
-
         fetch('http://localhost:3000/login', configObj)
             .then(response => response.json())
             .then(userData => {
                 //lifting state up tp parent (App) 
                 //once user signs in we want to make a fetch request to logged_in to set current_user 
-                this.props.handleLogin(userData);
+                props.handleLogin(userData);
             });
     }
 
-    render() {
-        return (
-            <div>
-                <Form>
-                    <Row>
-                        <Col xs={3}>
-                            <Form.Control type="text" placeholder="Username" value={this.state.username} onChange={this.onChange} name="username" />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={3} >
-                            <Form.Control type="text" placeholder="Password" value={this.state.password} onChange={this.onChange} name="password" />
-                            <Button variant="success" onClick={(event) => this.onClick(event)}> Submit </Button>
-                        </Col>
-                    </Row>
-                </Form>
-            </div>
-        );
-    }
-}
-
+    return (
+        <div>
+            <Form>
+                <Row>
+                    <Col xs={3}>
+                        <Form.Control type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} name="username" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={3} >
+                        <Form.Control type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" />
+                        <Button variant="success" onClick={(event) => onClick(event)}> Submit </Button>
+                    </Col>
+                </Row>
+            </Form>
+        </div>
+    );
+};
 export default LogIn;
