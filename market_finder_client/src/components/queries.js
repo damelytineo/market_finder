46dcc228cbf0db560fbac4c8dc52af9c94598aca
Mely07 <damely.tineo07@gmail.com>
2025-01-23 23:54:39 -0500

    initial setup:  GraphQL and Apollo Client

---
+++ market_finder_client/src/components/queries.js
@@ -0,0 +1,2 @@
+// add queries here or directly in the component(s) that need them
+// if here, export for use in components using the apollo useQuery hook
--- market_finder_client/src/index.js
+++ market_finder_client/src/index.js
@@ -4,14 +4,18 @@ import App from "../src/App";
 import "./output.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { ApolloProvider } from "@apollo/client";
+import client from './apolloClient'
 import store from "./store";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+       <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </Provider>
 );
